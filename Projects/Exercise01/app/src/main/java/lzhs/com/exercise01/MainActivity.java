c2c77aab6f49d4f904702ed0d31a786d399e48ca
LZHS <1050629507@qq.com>
2017-10-13 16:38:56 +0800

    lzhs 代码提交

--- Projects/Exercise01/app/src/main/java/lzhs/com/exercise01/MainActivity.java
+++ Projects/Exercise01/app/src/main/java/lzhs/com/exercise01/MainActivity.java
@@ -2,12 +2,17 @@ package lzhs.com.exercise01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
