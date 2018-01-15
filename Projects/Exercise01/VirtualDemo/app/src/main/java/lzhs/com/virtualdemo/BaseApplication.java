59b741ab8f5a3251cabea416690b0266b8144db3
LZHS <1050629507@qq.com>
2018-01-15 18:19:05 +0800

    lzhs 添加了VirtualDemo

---
+++ Projects/Exercise01/VirtualDemo/app/src/main/java/lzhs/com/virtualdemo/BaseApplication.java
@@ -0,0 +1,12 @@
+package lzhs.com.virtualdemo;
+
+import android.app.Application;
+
+/**
+ * <br/>
+ * 作者：LZHS<br/>
+ * 时间： 2018/1/15 16:53<br/>
+ * 邮箱：1050629507@qq.com
+ */
+public class BaseApplication extends Application {
+}
